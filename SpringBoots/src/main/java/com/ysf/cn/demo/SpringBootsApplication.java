3637cdeaa05bd0696d75bd47f6288dc0212b488f
ysfcore <1415034043@qq.com>
2021-09-16 23:25:57 +0800

    初始化springBoot

---
+++ SpringBoots/src/main/java/com/ysf/cn/demo/SpringBootsApplication.java
@@ -0,0 +1,13 @@
+package com.ysf.cn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootsApplication.class, args);
+    }
+
+}
---
+++ SpringBoots/src/test/java/com/ysf/cn/demo/SpringBootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysf.cn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
