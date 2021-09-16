a9031d590b932b5fedaec7412b2fbcdced4b6cac
ysfcore <1415034043@qq.com>
2021-09-16 23:50:30 +0800

    初始化springBoot

--- SpringBoots/src/main/java/com/ysf/cn/demo/SpringBootsApplication.java
+++ SpringBoots/src/main/java/com/ysf/cn/demo/SpringBootsApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootsApplication {
 
+    //token:  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
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootsApplication.class, args);
     }
 
